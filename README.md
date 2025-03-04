dca57d646cf47dff7a3e740b93fa7b945c9be591
brokenbyte <git@brokenbyte.io>
2025-03-03 22:52:47 -0700

    feat: support `+`, `?`, and `*` repetition mods

--- README.md
+++ README.md
@@ -3,4 +3,22 @@
 
 Create railroad diagrams from EBNF
 
-BNF specification in EBNF form from https://bnfplayground.pauliankline.com/ > Examples > BNF
+## Todo
+- support repitition specifiers and ranges (i.e. [a-z], +, *)
+    - `?`, `+`, and `*` now supported
+- support grouping
+- support rules spanning multiple lines
+- decide on the full syntax I want to use/support for EBNF.
+  See dwheeler.com, grammarware.net, w3.org, and cl.cam.ac.uk links below.
+- figure out if/how I want to handle bounded repitition
+- figure out if/how I want to handle negation
+- support escape characters
+
+## Credit
+Initial parser and `test/bnf.ebnf` file from BNF specification in EBNF form from https://bnfplayground.pauliankline.com/ > Examples > BNF
+
+Additional reading on EBNF syntax:
+https://www.cl.cam.ac.uk/~mgk25/iso-14977.pdf
+https://dwheeler.com/essays/dont-use-iso-14977-ebnf.html
+https://www.grammarware.net/text/2012/bnf-was-here.pdf
+https://www.w3.org/TR/xml/#sec-notation
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// #![allow(unused, clippy::all)]
+#![allow(unused, clippy::all)]
 
 use std::path::PathBuf;
 
@@ -6,6 +6,8 @@ use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
 
 use railroad as rr;
+
+type DynNode = Box<dyn rr::Node>;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
@@ -15,12 +17,13 @@ fn main() {
     let csv = std::fs::read_to_string("test/bnf.ebnf").unwrap();
 
     let mut result = EBNFParser::parse(Rule::syntax, &csv).unwrap();
+
     let trees = result.next().expect("expected root expr").into_inner();
 
     let nodes = trees
         .map(|p| {
             Box::new(rr::Sequence::new(vec![
-                Box::new(rr::SimpleStart) as Box<dyn rr::Node>,
+                Box::new(rr::SimpleStart) as DynNode,
                 Box::new(make_node(p)),
                 Box::new(rr::SimpleStart),
             ]))
@@ -43,7 +46,7 @@ fn make_node(pair: Pair<'_, Rule>) -> Box<dyn rr::Node> {
         R::rule => {
             let mut pair = pair.into_inner();
             let rule = pair.next().expect("no rule found");
-            let name = Box::new(rr::Comment::new(rule.as_str().to_owned())) as Box<dyn rr::Node>;
+            let name = Box::new(rr::Comment::new(rule.as_str().to_owned())) as DynNode;
 
             let expr = pair.next().expect("rule must have definition").into_inner();
             let mut rule_def = expr.map(parse_expr).collect::<Vec<_>>();
@@ -55,7 +58,7 @@ fn make_node(pair: Pair<'_, Rule>) -> Box<dyn rr::Node> {
             } else {
                 let seqs = rule_def
                     .into_iter()
-                    .map(|s| Box::new(rr::Sequence::new(s)) as Box<dyn rr::Node>)
+                    .map(|s| Box::new(rr::Sequence::new(s)) as DynNode)
                     .collect::<Vec<_>>();
                 let x = vec![name, Box::new(rr::Choice::new(seqs))];
 
@@ -68,7 +71,7 @@ fn make_node(pair: Pair<'_, Rule>) -> Box<dyn rr::Node> {
     }
 }
 
-fn parse_expr(pair: Pair<'_, Rule>) -> Vec<Box<dyn rr::Node>> {
+fn parse_expr(pair: Pair<'_, Rule>) -> Vec<DynNode> {
     use Rule as R;
 
     let node = match pair.as_rule() {
@@ -86,17 +89,41 @@ fn parse_term(pair: Pair<'_, Rule>) -> Box<dyn rr::Node> {
 
     match pair.as_rule() {
         R::term => {
-            let rule_or_lit = pair.into_inner().next().expect("term must have inner");
-            match rule_or_lit.as_rule() {
+            let mut pair = pair.into_inner();
+            let rule_or_lit = pair.next().expect("term must have inner");
+            let modifier = pair
+                .next()
+                .expect("parser guarantees a modifier here")
+                .into_inner()
+                .next();
+
+            let node: DynNode = match rule_or_lit.as_rule() {
                 R::rule_name => Box::new(rr::NonTerminal::new(rule_or_lit.as_str().to_owned())),
                 R::literal => Box::new(rr::Terminal::new(rule_or_lit.as_str().to_owned())),
                 _ => {
                     unreachable!("Got unsupported rule '{}' in term", rule_or_lit.as_str());
                 }
+            };
+            if let Some(m) = modifier {
+                match m.as_rule() {
+                    R::oper_cond => Box::new(rr::Optional::new(node)),
+                    R::oper_alo => Box::new(rr::Repeat::new(node, rr::Empty)),
+                    R::oper_rep => Box::new(rr::Optional::new(rr::Repeat::new(node, rr::Empty))),
+                    _ => unreachable!(),
+                }
+            } else {
+                node
             }
         }
         _ => {
             unreachable!("parse_expr unsupported rule: {:?}", pair.as_rule());
         }
     }
+}
+
+fn new_optional(node: DynNode) -> DynNode {
+    Box::new(rr::Optional::new(node))
+}
+fn new_repeat(node: DynNode, r: DynNode) -> DynNode {
+    Box::new(rr::Repeat::new(node, r))
 }

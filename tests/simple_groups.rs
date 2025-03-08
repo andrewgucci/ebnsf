144783140ac234f9d314ee7ca7a0c4f0f78b421c
brokenbyte <git@brokenbyte.io>
2025-03-08 16:38:01 -0700

    feat: properly handle grouped sequences this time

---
+++ tests/simple_groups.rs
@@ -0,0 +1,22 @@
+use ebnsf::parse_ebnf;
+
+mod common;
+use common::save_svg;
+
+#[test]
+fn simple_groups() {
+    let rule = "\
+<rule> ::= <foo> (<bar> \"|\" <bish> <bosh>)*
+<rule> ::= <foo> | (<bish> | <bosh>)
+<rule> ::= <foo> | (<bish> | <bosh>)?
+<rule> ::= <foo> | (<bish> | <bosh>)+
+<rule> ::= <foo> | (<bish> | <bosh>)*
+<rule> ::= (<foo> | (<bish> | <bosh>)*)?
+";
+
+    let svg = parse_ebnf(rule).expect("valid grammar").to_string();
+
+    save_svg("simple_groups", &svg);
+
+    insta::assert_snapshot!("simple_groups", svg);
+}
